0743a77d80ccdca16eb020a5fcd21eb657134967
Nagy Ábel Mihály <42240320+nagyabelmihaly@users.noreply.github.com>
2024-11-18 22:10:26 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,17 @@
+function skillsMember() {
+  var member = {};
+
+  member.skills = {
+    'HTML': 'Advanced',
+    'CSS': 'Advanced',
+    'JavaScript': 'Advanced',
+    'AngularJS': 'Intermediate',
+    'ReactJS': 'Intermediate',
+    'VueJS': 'Intermediate',
+    'NodeJS': 'Intermediate',
+    'MongoDB': 'Intermediate',
+    'MySQL': 'Intermediate'
+  };
+
+  return member;
+}
